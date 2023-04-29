30d652906f5dedcc43dd1b987caad38d78052a37
nurhatmurathan <nurhatmurathan001@gmail.com>
2023-04-29 23:11:35 +0600

    add hsl color to tile element

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ const game_board = document.getElementById("game-board");
 var board_area = 4;
 const grid = new Grid(game_board, board_area);
 grid.getRandomEmptyCell().linkTile(new Tile(game_board));
+grid.getRandomEmptyCell().linkTile(new Tile(game_board));
--- tile.js
+++ tile.js
@@ -2,8 +2,7 @@ export class Tile{
     constructor(gridElement){
         this.tileElement = document.createElement("div");
         this.tileElement.classList.add("tile");
-        this.value = Math.random() > 0.4 ? 2 : 4;
-        this.tileElement.textContent = this.value;
+        this.setValue(Math.random() > 0.4 ? 2 : 4);
         gridElement.append(this.tileElement);
     }
 
@@ -13,4 +12,13 @@ export class Tile{
         this.tileElement.style.setProperty("--y", y);
         this.tileElement.style.setProperty("--x", x);
     }
+
+    setValue(value){
+        this.value = value;
+        this.tileElement.textContent = this.value;
+        const bglightness = 100 - Math.log2(value) * 9;
+        this.tileElement.style.setProperty("--bg-lightness", `${bglightness}%`);
+        this.tileElement.style.setProperty("--text-lightness", `${bglightness < 50 ? 90 : 10}%`);
+    }
+    
 }
